65f91279c0f5c4dd783578364c25310ef1f0f816
sumit <roy184433@gmail.com>
2025-05-14 20:00:00 +0530

    com16

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\user;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        user::create([
+            'name' => 'Admin User', // Replace with the desired admin name
+            'email' => 'sumit.de08765431@gmail.com', // Admin email
+            'password' => bcrypt('123456789'), // Admin password (hashed)
+            'role' => 'admin', // Admin role
+            'phone' => null, // Set to null if not provided
+            'address' => null, // Set to null if not provided
+            'dep' => null, // Set to null if not provided
+        ]);
     }
 }
